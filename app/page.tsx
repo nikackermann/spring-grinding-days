cd49e1292ce87fec7caa685f54b5aba211f775d9
nackermann <nik.ackermann@gmail.com>
2024-01-09 21:23:29 -0500

    Update main container styles

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,10 @@ import { MovingBorder } from '@/components/ui/moving-border';
 
 export default function Home() {
     return (
-        <main className="h-screen overflow-hidden dark:bg-black bg-white dark:bg-grid-white/[0.12] bg-grid-black/[0.12] relative p-2">
+        <main className="h-screen overflow-auto dark:bg-black bg-white dark:bg-grid-white/[0.12] bg-grid-black/[0.12] relative p-2">
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] z-0"></div>
 
-            <div className="relative flex flex-col justify-center items-center min-h-screen z-10 -mt-12">
+            <div className="relative flex flex-col justify-start items-center pt-10 sm:pt-20 md:pt-32 lg:pt-40 xl:pt-48 min-h-screen z-10">
                 <div className="text-center space-y-2 mb-6">
                     <div className="flex justify-center items-center">
                         <span>
