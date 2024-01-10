733a0a1da9a92ebe61674f4cedc498d7d18761e9
nackermann <nik.ackermann@gmail.com>
2024-01-09 21:47:00 -0500

    Update main container overflow to hidden

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import { MovingBorder } from '@/components/ui/moving-border';
 
 export default function Home() {
     return (
-        <main className="h-screen overflow-auto dark:bg-black bg-white dark:bg-grid-white/[0.12] bg-grid-black/[0.12] relative p-2">
+        <main className="h-screen overflow-hidden dark:bg-black bg-white dark:bg-grid-white/[0.12] bg-grid-black/[0.12] relative p-2">
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] z-0"></div>
 
             <div className="relative flex flex-col justify-start items-center pt-10 sm:pt-20 md:pt-32 lg:pt-40 xl:pt-48 min-h-screen z-10">
