fe5a7bc2c87df96dfb797c5d2ce4e8e445916d5c
nackermann <nik.ackermann@gmail.com>
2024-01-10 17:41:36 -0500

    Update Success component to include an SVG icon and email message

--- components/ui/success.tsx
+++ components/ui/success.tsx
@@ -1,3 +1,21 @@
 export async function Success() {
-    return <div>Success</div>;
+    return (
+        <div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="green"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M5 13l4 4L19 7"
+                />
+            </svg>
+            Check your email for more information!
+        </div>
+    );
 }
