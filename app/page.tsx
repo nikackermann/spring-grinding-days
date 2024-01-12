99822cff2a3e3cb93505c039e8ebcb2c8bf4d287
nackermann <nik.ackermann@gmail.com>
2024-01-12 14:15:48 -0500

    Update CardHeader margin-top in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
                 </div>
                 <div className="w-full max-w-2xl">
                     <Card className="min-w-full">
-                        <CardHeader className="-mt-2">
+                        <CardHeader className="-mt-2 md:mt-1">
                             <div className="md:flex hidden space-x-10 justify-center">
                                 <Image
                                     src="/logo1.jpg"
